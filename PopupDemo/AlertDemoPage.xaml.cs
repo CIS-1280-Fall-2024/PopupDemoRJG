d44ecb84bff580e6b113d5f7ebf6e724fcde5288
Robert Garner <rgarner011235@gmail.com>
2024-10-15 18:13:06 -0600

    Completed alert demo

--- PopupDemo/AlertDemoPage.xaml.cs
+++ PopupDemo/AlertDemoPage.xaml.cs
@@ -1,3 +1,5 @@
+using System.Diagnostics;
+
 namespace PopupDemo;
 
 public partial class AlertDemoPage : ContentPage
@@ -6,4 +8,15 @@ public partial class AlertDemoPage : ContentPage
 	{
 		InitializeComponent();
 	}
+
+    private async void btnShowAlertClickedAsync(object sender, EventArgs e)
+    {
+        await DisplayAlert("Alert", "You have been alerted", "OK");
+    }
+
+    private async void btnShowConfirmDialogClickedAsync(object sender, EventArgs e)
+    {
+        bool answer = await DisplayAlert("Please confirm?", "Are you sure you want to delete?", "Yes", "No");
+        Debug.WriteLine("Answer: " + answer);
+    }
 }
